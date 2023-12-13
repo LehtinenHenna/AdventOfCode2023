5bae344cff6c0b6194c6bb7a6c6c534eb13f52c7
Henna Lehtinen <henna@iontau.com>
2023-12-13 18:01:20 +0200

    Finish part 1

--- day5/day5.py
+++ day5/day5.py
@@ -8,20 +8,107 @@ except ModuleNotFoundError:
 
 
 class SeedLocationMapper:
+    '''
+    Uses a given filepath to collect data and find the smallest 
+    location number that corresponds to any of the initial seed numbers.
+    More information in assignment5.md.
+    '''
 
     def __init__(self, puzzle_input_path):
         self.puzzle_input_path = puzzle_input_path
-        self.seeds = []
-        self.seed_to_soil_map = {} # key = seed number, value = soil number
-        self.soil_to_fertilizer_map = {} # key = soil number, value = fertilizer number
-        self.fertilizer_to_water_map = {} # key = fertilizer number, value = water number
-        self.water_to_light_map = {} # key = water, value = light
-        self.light_to_temperature_map = {} # key = light, value = temperature
-        self.temperature_to_humidity_map = {} # key = temperature, value = humidity
-        self.humidity_to_location_map = {} # key = humidity, value = location
+        self.seeds = [] # Seed numbers
+
+        # Each of the map lists contain embedded lists.
+        # Each embedded list contains destination range start number, 
+        # source range number and range length number in that order.
+        self.seed_to_soil_map = []
+        self.soil_to_fertilizer_map = []
+        self.fertilizer_to_water_map = []
+        self.water_to_light_map = []
+        self.light_to_temperature_map = []
+        self.temperature_to_humidity_map = []
+        self.humidity_to_location_map = []
+
+        # Used for organizing data in the right lists
+        self.map_identifiers = {
+            'seed-to-soil': False,
+            'soil-to-fertilizer': False,
+            'fertilizer-to-water': False,
+            'water-to-light': False,
+            'light-to-temperature': False,
+            'temperature-to-humidity': False,
+            'humidity-to-location': False
+        }
     
-    def seed_to_location_mapper(self):
-        pass
+    def main(self):
+        '''
+        Use internal methods and given filepath to find the smallest location number that 
+        corresponds to any of the initial seed numbers.
+        '''
+        self.create_maps()
+        locations = []
+        for seed in self.seeds:
+            location = self.seed_to_location_mapper(seed)
+            locations.append(location)
+        return min(locations)
+    
+    def seed_to_location_mapper(self, seed_number):
+        '''Find location number for a seed number.'''
+        soil_number = self.convert_number(seed_number, self.seed_to_soil_map)
+        fertilizer_number = self.convert_number(soil_number, self.soil_to_fertilizer_map)
+        water_number = self.convert_number(fertilizer_number, self.fertilizer_to_water_map)
+        light_number = self.convert_number(water_number, self.water_to_light_map)
+        temperature_number = self.convert_number(light_number, self.light_to_temperature_map)
+        humidity_number = self.convert_number(temperature_number, self.temperature_to_humidity_map)
+        location_number = self.convert_number(humidity_number, self.humidity_to_location_map)
+        return location_number
+    
+    def convert_number(self, source_number, map_list):
+        '''Convert given source number into a destination number according to a map list.'''
+        for lst in map_list:
+            destination_range_start = lst[0]
+            source_range_start = lst[1]
+            range_length = lst[2]
+            if  source_range_start <= source_number < (source_range_start + range_length):
+                # e.g., with map list [2, 44, 5] and source number 45, the destination number is 3
+                destination_number = source_number - source_range_start + destination_range_start
+                return destination_number
+        # no match was found, so the destination number and source number are the same
+        return source_number
 
     def create_maps(self):
-        pass
+        '''Collect data from the given filepath into lists.'''
+        for index, line in enumerate(file_line_reader(self.puzzle_input_path)):
+            # create seeds list
+            if line.startswith('seeds:'):
+                line = line.replace('seeds:', '')
+                self.seeds = line.split(' ')
+                self.seeds = [int(seed) for seed in self.seeds if seed.isdigit()]
+            else:
+                if len(line) > 0:
+                    if not line[0].isdigit():
+                        # the following lines of the file contain numbers for a new map
+                        for key, value in self.map_identifiers.items():
+                            if line.startswith(key):
+                                for indentifier in self.map_identifiers:
+                                    # Activate the current line identifier and deactivate all the others 
+                                    # to collect the numbers in the following lines of the file in the right map list
+                                    if key == indentifier:
+                                        self.map_identifiers[indentifier] = True
+                                    else:
+                                        self.map_identifiers[indentifier] = False
+                    else:
+                        # collect numbers in the activated list
+                        for key, value in self.map_identifiers.items():
+                            if value == True:
+                                # select the activated list
+                                map_list = getattr(self, key.replace('-', '_') + '_map')
+                        dest_source_range_list = line.split(' ')
+                        dest_source_range_list = [int(number_str) for number_str in dest_source_range_list]
+                        map_list.append(dest_source_range_list)
+                
+
+if __name__ == '__main__':
+    seed_location_mapper = SeedLocationMapper('puzzle-input5.txt')
+    smallest_location = seed_location_mapper.main()
+    print('Min location', smallest_location)
